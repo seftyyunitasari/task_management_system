1e072e53046297df17175bf6f645561db7d14f95
sefty <seftyyunitasari@gmail.com>
2022-01-24 11:29:31 +0900

    Add user_id in tasks

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 2022_01_20_064314) do
+ActiveRecord::Schema.define(version: 2022_01_24_022526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +11,8 @@ ActiveRecord::Schema.define(version: 2022_01_20_064314) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "priority"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +24,5 @@ ActiveRecord::Schema.define(version: 2022_01_20_064314) do
     t.index ["email"], name: "index_users_on_email"
   end
 
+  add_foreign_key "tasks", "users"
 end
